2c2b93089cb4dd718bb817988260ba1b1666520d
river99 <qkrrkd42@naver.com>
2022-08-28 19:00:34 +0900

    leak file

---
+++ app/src/main/java/com/example/LoginInfo.java
@@ -0,0 +1,26 @@
+package com.example;
+
+
+import com.example.team20.domain.Member;
+
+    public class LoginInfo {
+
+    private static final LoginInfo loginInfo = new LoginInfo();
+        private Member currentMember;
+
+        public Member getCurrentMember() {
+            return currentMember;
+        }
+        public void setCurrentMember(Member currentMember) {
+            this.currentMember = currentMember;
+        }
+
+    public static LoginInfo getInstance(){
+        return loginInfo;
+    }
+
+    private LoginInfo(){
+
+    }
+
+}
